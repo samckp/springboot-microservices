c159feeb9cc33c4095c462c1bb04ce6c3cc02e84
samckp <sam.mca.liet@gmail.com>
2022-12-12 09:13:27 +0530

    updated

--- user-service/src/main/java/com/micro/userservice/external/services/RatingService.java
+++ user-service/src/main/java/com/micro/userservice/external/services/RatingService.java
@@ -1,2 +1,24 @@
-package com.micro.userservice.external.services;public interface RatingService {
+package com.micro.userservice.external.services;
+
+import com.micro.userservice.entities.Rating;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@Service
+@FeignClient(name = "RATING-SERVICE")
+public interface RatingService {
+
+    @PostMapping("/rating")
+    public ResponseEntity<Rating> create(Rating values);
+
+    @PutMapping("/rating/{ratingId}")
+    public ResponseEntity<Rating> updateRating(@PathVariable("ratingId") String ratingId, Rating rating );
+
+    @DeleteMapping("/rating/{ratingId}")
+    public void deleteRating(@PathVariable("ratingId") String ratingId);
 }
--- user-service/src/test/java/com/micro/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/micro/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,29 @@
 package com.micro.userservice;
 
+import com.micro.userservice.entities.Rating;
+import com.micro.userservice.external.services.RatingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+	@Autowired
+	RatingService ratingService;
+
 	@Test
-	void contextLoads() {
+	void createRating(){
+
+		String expected = "201 CREATED";
+		Rating rating = Rating.builder().rating(8).userId("").hotelId("").feedback("Good one").build();
+		ResponseEntity<Rating> responseRating = ratingService.create(rating);
+
+		HttpStatus resp = responseRating.getStatusCode();
+		assertEquals(expected, resp.toString());
 	}
 
 }
